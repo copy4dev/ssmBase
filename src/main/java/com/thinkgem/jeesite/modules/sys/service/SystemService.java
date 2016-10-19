2df9dcd1ccb3bea57a9aa914cf8a18fcd38a6f51
copy4dev <copy4dev@126.com>
2016-10-19 09:19:51 +0800

    orign

--- src/main/java/com/thinkgem/jeesite/modules/sys/service/SystemService.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/service/SystemService.java
@@ -400,10 +400,10 @@ public class SystemService extends BaseService implements InitializingBean {
 	 */
 	public static boolean printKeyLoadMessage(){
 		StringBuilder sb = new StringBuilder();
-		sb.append("\r\n======================================================================\r\n");
-		sb.append("\r\n    欢迎使用 "+Global.getConfig("productName")+"  - Powered By http://jeesite.com\r\n");
-		sb.append("\r\n======================================================================\r\n");
-		System.out.println(sb.toString());
+		sb.append("======================================================================\r\n");
+		sb.append("    欢迎使用 "+Global.getConfig("productName")+"  - Powered By http://jeesite.com\r\n");
+		sb.append("======================================================================");
+		System.err.println(sb.toString());
 		return true;
 	}
 	
