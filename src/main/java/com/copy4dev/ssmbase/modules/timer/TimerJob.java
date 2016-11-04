d63fce13a31e36a01652232e9c7f597c83809b9f
copy4dev <copy4dev@126.com>
2016-11-04 15:42:48 +0800

    orign

--- src/main/java/com/copy4dev/ssmbase/modules/timer/TimerJob.java
+++ src/main/java/com/copy4dev/ssmbase/modules/timer/TimerJob.java
@@ -3,9 +3,12 @@ package com.copy4dev.ssmbase.modules.timer;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.copy4dev.ssmbase.common.db.DynamicDataSource;
+import com.copy4dev.ssmbase.modules.sys.service.DictService;
 import com.copy4dev.ssmbase.modules.utils.HttpPostUtils;
 
 /**
@@ -14,15 +17,43 @@ import com.copy4dev.ssmbase.modules.utils.HttpPostUtils;
 @Component
 public class TimerJob {
 
-	@Scheduled(cron = "*/10 * * 6 * ?")
+	@Autowired
+	private DictService dictService;
+
+	@Scheduled(cron = "*/10 * 0 * * ?")
 	public void test() {
 
 		System.out.println("=======定时任务示例=======");
 
 		httpPostTest(false);
+
+		chDBTest(false);
 	}
 
-	public void httpPostTest(boolean isRun) {
+	/**
+	 * 数据源切换例子
+	 * 
+	 * @param isRun
+	 */
+	private void chDBTest(boolean isRun) {
+		if (!isRun)
+			return;
+		// 切换数据源dataSource2nd
+		DynamicDataSource.setCurrentLookupKey("dataSource2nd");
+
+		System.out.println("=======你的业务=======");
+
+		// 切换回默认数据源dataSourceDef
+		DynamicDataSource.setCurrentLookupKey("dataSourceDef");
+
+	}
+
+	/**
+	 * http测试
+	 * 
+	 * @param isRun
+	 */
+	private void httpPostTest(boolean isRun) {
 		if (!isRun)
 			return;
 
@@ -32,7 +63,7 @@ public class TimerJob {
 		parametersMap.put("test_key", "123");
 		parametersMap.put("method", "test");
 
-		//提交数据
+		// 提交数据
 		String result = HttpPostUtils.excuteRequest(url, parametersMap);
 
 		System.out.println("result:\n" + result);
